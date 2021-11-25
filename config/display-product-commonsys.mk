b552562e284d7649bc8c7aefc29056fd2763bca6
palaych <palaychm@yandex.ru>
2021-11-26 03:29:12 +0900

    display-commonsys: Add vendor variant for display packages
     * Display HAL's request this.
     * Were lost after moved in vendor/qcom/opensource/commonsys/display
    
    Change-Id: I900f91662ddec7e9f7c1c7c53663a5691f1a6326

--- config/display-product-commonsys.mk
+++ config/display-product-commonsys.mk
@@ -1,10 +1,13 @@
-PRODUCT_PACKAGES += libqdMetaData.system \
+PRODUCT_PACKAGES += libqdMetaData \
+                    libqdMetaData.system \
+                    libgralloc.qti \
                     libgralloc.system.qti \
                     libdrm \
                     libsmomo.qti \
                     liblayerext.qti \
                     libsmomoconfig.qti \
                     libcomposerextn.qti \
+                    libdisplayconfig.qti \
                     libdisplayconfig.system.qti
 
 SOONG_CONFIG_NAMESPACES += qtidisplaycommonsys
